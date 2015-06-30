43c8bf8bd7ced9799eec784fd4eb5740c4e32003
Mateus Sobrinho <chocsx@hotmail.com>
2015-06-30 00:17:55 -0300

    mobile init

---
+++ js/script.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    var pull        = $('.menu-icon');
+        menu        = $('nav ul');
+        menuHeight  = menu.height();
+ 
+    $(pull).on('click', function(e) {
+        e.preventDefault();
+        menu.slideToggle();
+    });
+});
