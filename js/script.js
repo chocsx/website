bc8225c9b604a4eca938acaab884a173b6f7ecad
Mateus Sobrinho <chocsx@hotmail.com>
2015-07-01 00:21:53 -0300

    title over images slider

--- js/script.js
+++ js/script.js
@@ -9,5 +9,10 @@ $(document).ready(function(){
         menu.slideToggle();
     });
 
-
+		  $("#owl-example").owlCarousel({
+			  navigation : true, // Show next and prev buttons
+		      slideSpeed : 300,
+		      paginationSpeed : 400,
+		      singleItem:true
+		  });   
 });
